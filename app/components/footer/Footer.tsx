c41996b097efff6709df035ef35f3a025ea545a7
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-07 17:31:42 +0100

    finishing footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,8 +1,31 @@
+import { CopyrightText, FooterWrapper, Left, Right, SocialMedia, StyledLink } from "./styles/Styles"
+import { AiFillGithub, AiFillLinkedin, AiOutlineInstagram, AiFillTwitterCircle } from "react-icons/ai";
+
 function Footer() {
   return (
-    <div>
-        Fouter
-    </div>
+    <FooterWrapper>
+        <Left>
+          <CopyrightText>
+            &copy; 2001-2022 AbdeNassar Amimi
+          </CopyrightText>
+        </Left>
+        <Right>
+          <SocialMedia>
+            <StyledLink href="https://github.com" target="_blank">
+              <AiFillGithub size={ 26 } />
+            </StyledLink> 
+            <StyledLink href="https://linkedin.com" target="_blank">
+              <AiFillLinkedin size={ 26 } />
+            </StyledLink> 
+            <StyledLink href="https://instagram.com" target="_blank">
+              <AiOutlineInstagram size={ 26 } />
+            </StyledLink> 
+            <StyledLink href="https://twitter.com" target="_blank">
+              <AiFillTwitterCircle size={ 26 } />
+            </StyledLink>
+          </SocialMedia>
+        </Right>
+    </FooterWrapper>
   )
 }
 
--- app/components/footer/styles/Styles.tsx
+++ app/components/footer/styles/Styles.tsx
@@ -1,5 +1,35 @@
 import styled from "styled-components";
 
 export const FooterWrapper = styled.footer`
+    display: flex;
+    justify-content: space-between;
+    padding: 0 40px;
+    margin-top: -10px;
+    align-items: center;
+`
+
+export const Left = styled.div`
     
+`
+
+export const Right = styled.div`
+    
+`
+
+export const SocialMedia = styled.div`
+    display: flex;
+    gap: 10px;
+`
+
+export const CopyrightText = styled.div`
+    color: #2B2B2B;
+    font-size: 12px;
+`
+
+export const StyledLink = styled.a`
+    color : #01B3FF;
+
+    &:hover{
+        color : #027AFC;
+    }
 `
