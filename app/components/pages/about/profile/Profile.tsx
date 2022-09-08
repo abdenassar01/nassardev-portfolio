07e62e03f28315442a041f4c1c05d229601025eb
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-08 09:23:17 +0100

    finishing profile

--- app/components/pages/about/profile/Profile.tsx
+++ app/components/pages/about/profile/Profile.tsx
@@ -1,6 +1,36 @@
+import { AiFillDribbbleCircle, AiFillFacebook, AiFillGithub, AiFillLinkedin, AiFillTwitterCircle, AiOutlineInstagram } from "react-icons/ai"
+import { A, Header, Image, MainInfo, NameHeading, Section, SocialMediaLinks } from "./styles/Styles"
+
 function Profile() {
   return (
-    <div>Profile</div>
+    <Section>
+      <Header>
+        <MainInfo>
+          <Image src="https://i.imgur.com/t8Z6gHr.png" alt="ABDENASSAR AMIMI" />
+          <NameHeading>ABDENASSAR AMIMI</NameHeading>
+          <SocialMediaLinks>
+            <A href="https://github.com/abdenassar01" target="_blank">
+              <AiFillGithub size={ 20 } /> 
+            </A> 
+            <A href="https://linkedin.com/in/abdenassar01" target="_blank">
+              <AiFillLinkedin size={ 20 } /> 
+            </A> 
+            <A href="https://instagram.com/nassardev" target="_blank">
+              <AiOutlineInstagram size={ 20 } /> 
+            </A> 
+            <A href="https://twitter.com/abdenassar01" target="_blank">
+              <AiFillTwitterCircle size={ 20 } /> 
+            </A> 
+            <A href="https://facebook.com/abdenassar01" target="_blank">
+              <AiFillFacebook size={ 20 } /> 
+            </A> 
+            <A href="https://dribbble.com/abdenassar01" target="_blank">
+              <AiFillDribbbleCircle size={ 20 } /> 
+            </A>
+          </SocialMediaLinks>
+        </MainInfo>
+      </Header>
+    </Section>
   )
 }
 
--- app/components/pages/about/profile/styles/Styles.tsx
+++ app/components/pages/about/profile/styles/Styles.tsx
@@ -1,2 +1,51 @@
 import styled from "styled-components";
+import { PrimaryColors } from "~/util";
 
+export const Section = styled.section`
+    margin-top: 50px;
+`
+
+export const Header = styled.div`
+    background: linear-gradient(90deg, ${ PrimaryColors["100"] } 10%, ${ PrimaryColors["200"] } 100%);
+    width: 100%;
+    height: 60px;
+    border-bottom: 1px solid #000A61;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    margin-bottom: 100px;
+`
+
+export const MainInfo = styled.div`
+    margin-top: 30px;
+    display: grid;
+    place-items: center;
+`
+
+export const Image = styled.img`
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+`
+
+export const NameHeading = styled.h3`
+    font-size: 16px;
+    background: -webkit-linear-gradient(30deg, ${ PrimaryColors["100"] } 10%, ${ PrimaryColors["200"] } 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+` 
+
+export const SocialMediaLinks = styled.div`
+    display: flex;
+    margin-top: 5px;
+    gap: 10px;
+`
+
+export const A = styled.a`
+    color: ${ PrimaryColors["100"] };
+
+    &:hover{
+        color: ${ PrimaryColors["200"] };
+    }
+`
