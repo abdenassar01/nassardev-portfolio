7402e33ae34f085402d6d9aa4c461b816da43d9c
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-08 09:23:58 +0100

    fixing isues in design

---
+++ app/components/pages/about/text/styles/Styles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.article`
+    padding: 0 60px;
+`
+
+export const Paragraph = styled.p`
+    margin-top: 20px;
+`
--- app/util/globalStyles.tsx
+++ app/util/globalStyles.tsx
@@ -10,6 +10,5 @@ export const GlobalStyles = createGlobalStyle`
     body{
         font-family: 'Roboto', sans-serif;
         font-style: 16px;
-        padding: 0 20px;
     }
 `
