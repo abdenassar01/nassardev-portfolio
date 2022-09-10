4365f188bbd09d3fa2766d4ee4164987fcacb62f
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-10 15:09:05 +0100

    finishing error page

--- app/components/pages/error/Error.tsx
+++ app/components/pages/error/Error.tsx
@@ -1,11 +1,24 @@
+import { useCatch } from "@remix-run/react";
+import { MainTitle } from "~/util"
 import Illustration from "./404/Illustration"
+import { ErrorText, ErrorWrapper, Paragraph } from "./styles/Styles"
 
 function Error() {
+
+  const caught = useCatch();
+
   return (
-    <div>
+    <ErrorWrapper>
         <Illustration />
-        Error handler 
-    </div>
+        <ErrorText>
+          <MainTitle>
+              { caught.status } { caught.statusText }
+          </MainTitle>
+          <Paragraph>
+            We don't seems to find the page you are looking for
+          </Paragraph>
+        </ErrorText>
+    </ErrorWrapper>
   )
 }
 
