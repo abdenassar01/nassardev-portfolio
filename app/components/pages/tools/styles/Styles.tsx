93e0bf2a0a466b45a871c4e71a4c3ead19934743
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-11 11:37:36 +0100

    making tools page responsive 📱

--- app/components/pages/tools/styles/Styles.tsx
+++ app/components/pages/tools/styles/Styles.tsx
@@ -4,13 +4,22 @@ import styled from "styled-components";
 export const CentredWrapper = styled.main`
     display: flex;
     justify-content: center;
+    min-height: 83vh;
 `
 
 export const Wrapper = styled.div`
     margin-top: 50px;
     padding: 0 40px;
+
+    @media screen and (max-width: 768px) {
+        padding: 0 20px;
+    }
 `
 
 export const Paragraph = styled.p`
     padding: 20px;
+
+    @media screen and (max-width: 768px) {
+        font-size: 14px;
+    }
 `
