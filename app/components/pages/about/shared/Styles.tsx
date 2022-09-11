59f961a3e7dc3e35121e740bffac928e1fa7199f
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-11 11:33:33 +0100

    making about me page fully responsive 🏝️📱

--- app/components/pages/about/shared/Styles.tsx
+++ app/components/pages/about/shared/Styles.tsx
@@ -8,9 +8,17 @@ export const Heading = styled.h1`
     background-clip: text;
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
+
+    @media screen and (max-width: 668px) {
+        font-size: 20px;
+    }
 `
 
 export const SecondaryHeading = styled.h2`
     font-size: 18px;
     font-weight: bold;
+
+    @media screen and (max-width: 667px) {
+        font-size: 14px;
+    }
 `
--- app/components/pages/about/text/styles/Styles.tsx
+++ app/components/pages/about/text/styles/Styles.tsx
@@ -2,8 +2,18 @@ import styled from "styled-components";
 
 export const Wrapper = styled.article`
     padding: 0 60px;
+
+
+    @media screen and (max-width: 668px) {
+        padding: 0 20px;
+    }
 `
 
 export const Paragraph = styled.p`
     margin-top: 20px;
+
+
+    @media screen and (max-width: 668px) {
+        font-size: 14px;
+    }
 `
