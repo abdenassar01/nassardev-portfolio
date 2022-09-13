9689ac0607319a115b73994ebfe595017921587d
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-13 11:35:07 +0100

    adding react-form-hooks to validate form

--- app/components/pages/contact/Contact.tsx
+++ app/components/pages/contact/Contact.tsx
@@ -1,8 +1,29 @@
+import { useForm } from "react-hook-form"
 import { Field, Input, Label, Submit, TextArea } from "~/util"
 import Blob from "./blob/Blob"
-import { ContactWrapper, Illustration, LeftSide, RightSide, Raw, Form, SubmitField } from "./styles/Styles"
+import { ContactWrapper, Illustration, LeftSide, RightSide, Raw, Form, SubmitField, ErrorMessage } from "./styles/Styles"
+
+type MessageInfo = {
+    firstname: string,
+    lastname: string,
+    mail: string,
+    message: string
+}
 
 function Contact() {
+
+    const { register, handleSubmit, formState : { errors }, reset } = useForm<MessageInfo>() 
+
+    const onSubmit = (data: MessageInfo) => {
+        console.log(data);
+        reset({
+            firstname: "",
+            lastname: "",
+            mail: "",
+            message: ""
+        })
+    } 
+    
   return (
     <ContactWrapper>
         <LeftSide>
@@ -11,24 +32,28 @@ function Contact() {
             </Illustration>
         </LeftSide>
         <RightSide>
-            <Form>
+            <Form onSubmit={ handleSubmit( onSubmit ) }>
                 <Raw>
                     <Field>
-                        <Label htmlFor="firstname">Name: </Label>
-                        <Input type="text" id="firstname" name="firstname" />
+                        <Label htmlFor="firstname">{ errors.firstname ? <ErrorMessage>firstname is required. </ErrorMessage> : "Firstname:"  }</Label>
+                        <Input type="text" id="firstname" { ...register("firstname", { required: true })} />
                     </Field>                    
                     <Field>
-                        <Label htmlFor="lastname">Lastname: </Label>
-                        <Input type="text" id="lastname" name="lastname" />
+                        <Label htmlFor="lastname" >{ errors.lastname ? <ErrorMessage>lastname is required. </ErrorMessage> : "Lastname:"  } </Label>
+                        <Input type="text" id="lastname" { ...register("lastname", { required: true })} />
                     </Field>
                 </Raw>  
                 <Field>
-                    <Label htmlFor="mail">E-mail: </Label>
-                    <Input type="mail" id="maile" name="mail" />
+                    <Label htmlFor="mail" >{ errors.mail ? <ErrorMessage>{(errors.mail?.type === 'required') && "mail is required." || ( errors.mail?.message ) }</ErrorMessage> : "E-mail:"  } </Label>
+                    <Input type="mail" id="mail" { ...register("mail", { required: true, 
+                        pattern: {
+                                value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+                                message: "email format not valid."
+                            }})} />
                 </Field>                    
                 <Field>
-                    <Label htmlFor="message">Message: </Label>
-                    <TextArea id="message" name="message" rows={ 8 } />
+                    <Label htmlFor="message" >{ errors.message ? <ErrorMessage>message is required. </ErrorMessage> : "Message:"  }</Label>
+                    <TextArea id="message" rows={ 8 } { ...register("message", { required: true })} />
                 </Field>     
                 <SubmitField>
                     <Submit type="submit" value="Send" />
--- app/util/formUtilities.tsx
+++ app/util/formUtilities.tsx
@@ -17,9 +17,9 @@ export const Field = styled.div`
 export const Label = styled.label`
     font-size: 10px;
     font-weight: bold;
-    color: #A4A4A4;
     margin-top: 5px;
     margin-left: 5px;
+    color: ${ props => props.isError ? "red" : "#A4A4A4" };
 `
 
 export const Input = styled.input`
