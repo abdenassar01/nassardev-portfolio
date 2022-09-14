8aa990108bfd61f29f5f2cb72769cba381ee86b5
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-14 17:46:53 +0100

    adding animations to home page

--- app/components/pages/home/greeting/Greeting.tsx
+++ app/components/pages/home/greeting/Greeting.tsx
@@ -1,21 +1,25 @@
+import { motion } from "framer-motion"
 import {  MainTitle, PrimaryButton, PrimaryColors, SecondaryButton } from "~/util"
 import { Section, P, Wrapper, ButtonsWrapper } from "./styles/Styles"
 
 function Greeting() {
   return (
-    <Wrapper>
-      <Section>
-            Hi 👋 I'm
-            <MainTitle>Abdenassar Amimi</MainTitle>
-            <P>
-              I’m a full stack developer from morocco  who is passionate about using technology to empower people.
-            </P>
-        </Section>
-        <ButtonsWrapper>
-          <PrimaryButton to="/contact" color="white">Hire Me</PrimaryButton>
-          <SecondaryButton to="/about" color={ PrimaryColors["200"] }>About Me</SecondaryButton>
-        </ButtonsWrapper>
-    </Wrapper>
+    <motion.div animate={{x: 0}} initial={{x: -500}}>
+      <Wrapper>
+        <Section>
+              Hi 👋 I'm
+              <MainTitle>Abdenassar Amimi</MainTitle>
+              <P>
+                I’m a full stack developer from morocco  who is passionate about using technology to empower people.
+              </P>
+          </Section>
+          <ButtonsWrapper>
+            <PrimaryButton to="/contact" color="white">Hire Me</PrimaryButton>
+            <SecondaryButton to="/about" color={ PrimaryColors["200"] }>About Me</SecondaryButton>
+          </ButtonsWrapper>
+      </Wrapper>
+    </motion.div>
+    
     
   )
 }
